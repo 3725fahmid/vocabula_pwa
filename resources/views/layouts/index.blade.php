9e0b9ae874622d2dbd7f6119296cfdbbc35c0680
fahmid <fahmidhasankonok@gmail.com>
2026-01-23 00:57:03 +0600

    feat(homepage): add story title

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -12,10 +12,10 @@
                 
                 <div class="card-header border pt-3 px-3 d-flex align-items-center">
                     <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center text-white fw-bold" style="width: 40px; height: 40px; background: linear-gradient(45deg, #6a11cb 0%, #2575fc 100%);">
-                        S
+                        {{ $item['story_id'] }}
                     </div>
                     <div class="ms-2">
-                        <h6 class="mb-0 fw-bold text-dark">Story #{{ $item['story_id'] }}</h6>
+                        <h6 class="mb-0 fw-bold text-dark">{{ $item['title'] }}</h6>
                         {{-- <small class="text-muted">Classic • <i class="bi bi-globe-americas"></i></small> --}}
                     </div>
                 </div>
