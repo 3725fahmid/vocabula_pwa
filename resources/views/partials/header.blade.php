f6fe5c5e007fe49082a94187b6cf3191608441a5
fahmid <fahmidhasankonok@gmail.com>
2026-02-11 23:33:11 +0600

    add header margin top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-  <header id="page-topbar" class="mx-2 my-0 rounded-pill shadow-lg">
+  <header id="page-topbar" class="mx-2 mt-1 mt-md-0 rounded-pill shadow-lg">
     <div class="navbar-header d-flex justify-between">
 
         <div class="d-flex">
